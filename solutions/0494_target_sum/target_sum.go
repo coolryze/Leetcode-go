8dfc6bf5e70d0c8d8d5922c1188a8ecd22cf15cd
heyuze <ryzehe@gmail.com>
2024-08-12 11:36:33 +0800

    Update solution for problem 0494

--- solutions/0494_target_sum/target_sum.go
+++ solutions/0494_target_sum/target_sum.go
@@ -1,29 +1,77 @@
 package targetsum
 
-import "math"
-
 func findTargetSumWays(nums []int, target int) int {
 	total := 0
 	for _, num := range nums {
 		total += num
 	}
 
-	if int(math.Abs(float64(target))) > total {
+	diff := total - target
+	if diff < 0 || diff%2 == 1 {
 		return 0
 	}
-	if (total+target)%2 == 1 {
-		return 0
-	}
+	reg := diff / 2
 
-	bagWeight := (total + target) / 2
-	dp := make([]int, bagWeight+1)
+	dp := make([]int, reg+1)
 	dp[0] = 1
 
 	for i := 0; i < len(nums); i++ {
-		for j := bagWeight; j >= nums[i]; j-- {
+		for j := reg; j >= nums[i]; j-- {
 			dp[j] += dp[j-nums[i]]
 		}
 	}
 
-	return dp[bagWeight]
+	return dp[reg]
+}
+
+func findTargetSumWays2(nums []int, target int) int {
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+
+	diff := total - target
+	if diff < 0 || diff%2 == 1 {
+		return 0
+	}
+	reg := diff / 2
+
+	dp := make([][]int, len(nums)+1)
+	for i := range dp {
+		dp[i] = make([]int, reg+1)
+	}
+	dp[0][0] = 1
+
+	for i := 1; i <= len(nums); i++ {
+		for j := 0; j <= reg; j++ {
+			num := nums[i-1]
+			if j >= num {
+				dp[i][j] = dp[i-1][j] + dp[i-1][j-num]
+			} else {
+				dp[i][j] = dp[i-1][j]
+			}
+		}
+	}
+
+	return dp[len(nums)][reg]
+}
+
+func findTargetSumWays3(nums []int, target int) int {
+	res := 0
+
+	var backtrack func(index, sum int)
+	backtrack = func(index, sum int) {
+		if index == len(nums) {
+			if sum == target {
+				res++
+			}
+		} else {
+			backtrack(index+1, sum+nums[index])
+			backtrack(index+1, sum-nums[index])
+		}
+	}
+
+	backtrack(0, 0)
+
+	return res
 }
