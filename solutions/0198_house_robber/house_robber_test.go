6096b9ea03265cdf19fe9702d6da76ab96221e07
heyuze <ryzehe@gmail.com>
2024-08-14 19:17:44 +0800

    Add solution for problem 0198

---
+++ solutions/0198_house_robber/house_robber_test.go
@@ -0,0 +1,39 @@
+package houserobber
+
+import (
+	"reflect"
+	"testing"
+)
+
+type testcase struct {
+	input
+	output
+}
+
+type input struct {
+	nums []int
+}
+
+type output struct {
+	expected int
+}
+
+func TestRob(t *testing.T) {
+	tcs := []testcase{
+		testcase{
+			input{[]int{1, 2, 3, 1}},
+			output{4},
+		},
+		testcase{
+			input{[]int{2, 7, 9, 3, 1}},
+			output{12},
+		},
+	}
+
+	for _, tc := range tcs {
+		got := rob(tc.input.nums)
+		if !reflect.DeepEqual(got, tc.output.expected) {
+			t.Errorf("test case failed: input=%v, expected=%v, got=%v", tc.input, tc.output.expected, got)
+		}
+	}
+}
