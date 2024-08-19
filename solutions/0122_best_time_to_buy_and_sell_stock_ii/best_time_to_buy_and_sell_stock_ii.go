61bb1c69facf563ec3cc6872933a8e5e7c2d9de3
heyuze <ryzehe@gmail.com>
2024-08-19 16:57:00 +0800

    Update solution for problem 0122

--- solutions/0122_best_time_to_buy_and_sell_stock_ii/best_time_to_buy_and_sell_stock_ii.go
+++ solutions/0122_best_time_to_buy_and_sell_stock_ii/best_time_to_buy_and_sell_stock_ii.go
@@ -1,6 +1,22 @@
 package besttimetobuyandsellstockii
 
 func maxProfit(prices []int) int {
+	dp := make([][]int, len(prices))
+	for i, _ := range dp {
+		dp[i] = make([]int, 2)
+	}
+	dp[0][0] = -prices[0]
+	dp[0][1] = 0
+
+	for i := 1; i < len(prices); i++ {
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]-prices[i])
+		dp[i][1] = max(dp[i-1][1], dp[i-1][0]+prices[i])
+	}
+
+	return max(dp[len(prices)-1][0], dp[len(prices)-1][1])
+}
+
+func maxProfit2(prices []int) int {
 	res := 0
 
 	for i := 1; i < len(prices); i++ {
