7952f2558c6d9ba29b40a968289afde12b808d5c
heyuze <ryzehe@gmail.com>
2024-08-22 16:50:00 +0800

    Update solution for problem 0053

--- solutions/0053_maximum_subarray/maximum_subarray.go
+++ solutions/0053_maximum_subarray/maximum_subarray.go
@@ -1,8 +1,25 @@
 package maximumsubarray
 
-import "math"
+import (
+	"math"
+)
 
 func maxSubArray(nums []int) int {
+	res := nums[0]
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		dp[i] = max(dp[i-1]+nums[i], nums[i])
+		if dp[i] > res {
+			res = dp[i]
+		}
+	}
+
+	return res
+}
+
+func maxSubArray2(nums []int) int {
 	res := math.MinInt
 
 	sum := 0
